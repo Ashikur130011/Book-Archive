9cccb111fc37a5b6282bfbfefed1dbbeaa47bca5
Ashikur Rahman <smashikurrahman24@gmail.com>
2021-09-02 12:28:28 +0600

    decorated

--- book.js
+++ book.js
@@ -1,41 +1,39 @@
 const errorMessage = document.getElementById('error-msg')
+    /* get Input Value */
 const searchBook = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    searchField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayBooks(data.docs));
-    document.getElementById('bookSelf').textContent = '';
+	const searchField = document.getElementById('search-field');
+	const searchText = searchField.value;
+	searchField.value = '';
+	const url = `http://openlibrary.org/search.json?q=${searchText}`;
+	fetch(url)
+		.then(res => res.json())
+		.then(data => displayBooks(data.docs));
+	document.getElementById('bookSelf').textContent = '';
 }
 
 const displayBooks = books => {
-    const bookLength = document.getElementById('book-length');
-    /* show book length */
-    if(books.length === 0) {
-        errorMessage.classList.remove('d-none');
-        bookLength.textContent="";
-    }
-    if(books.length){
-        
-    
-        bookLength.innerHTML = `Total Book Found: ${books.length}`;
-    errorMessage.classList.add('d-none');
-    }
-    
+	const bookLength = document.getElementById('book-length');
+	/* show book length */
+	if (books.length === 0) {
+		errorMessage.classList.remove('d-none');
+		bookLength.textContent = "";
+	}
+	if (books.length) {
+		bookLength.innerHTML = `Total Book Found: ${books.length}`;
+		errorMessage.classList.add('d-none');
+	}
 
-    /* show book card */
-    const bookSelf = document.getElementById('bookSelf');
-    books.forEach(book => {
+	/* show book card */
+	const bookSelf = document.getElementById('bookSelf');
+	books.forEach(book => {
 
-        /* default text for null found */
-    if(
-        book.cover_i === undefined || book.author_name === undefined || book.publisher === undefined || book.first_publish_year === undefined
-    ) {
-        const makeDiv = document.createElement('div');
-        makeDiv.classList.add('col');
-        makeDiv.innerHTML = `
+		/* default text for null found */
+		if (
+			book.cover_i === undefined || book.author_name === undefined || book.publisher === undefined || book.first_publish_year === undefined
+		) {
+			const makeDiv = document.createElement('div');
+			makeDiv.classList.add('col');
+			makeDiv.innerHTML = `
         
         <div class="card h-100 border border-info rounded mx-auto text-center text-danger">
             <img src=" " class="card-img-top img-fluid h-75 text-danger " alt="There is no image">
@@ -45,14 +43,13 @@ const displayBooks = books => {
             <h3 class="card-text">First Publish Year: there is no record</h3>
             </div>
         `;
-        bookSelf.appendChild(makeDiv);
-    ;
-    }
-        /* Dynamic text for result found */
-    else {
-        const makeDiv = document.createElement('div');
-        makeDiv.classList.add('col');
-        makeDiv.innerHTML = `
+			bookSelf.appendChild(makeDiv);;
+		}
+		/* Dynamic text for result found */
+		else {
+			const makeDiv = document.createElement('div');
+			makeDiv.classList.add('col');
+			makeDiv.innerHTML = `
         
         <div class="card h-100 border border-info rounded mx-auto text-center">
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid h-75" alt="Cover Page">
@@ -62,8 +59,8 @@ const displayBooks = books => {
             <h3 class="card-text">First Publish Year: ${book.first_publish_year}</h3>
             </div>
         `;
-        bookSelf.appendChild(makeDiv);
-    
-    }
-});
+			bookSelf.appendChild(makeDiv);
+
+		}
+	});
 }
